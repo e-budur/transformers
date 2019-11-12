99f5bbcbacd78ea4e0397a650f95e0e1866eed22
Emrah Budur <emrah.budur@yahoo.com>
2019-11-12 22:26:41 +0300

    Substituted the mistyped variable for num_intent_labels

--- transformers/modeling_bert_nlu.py
+++ transformers/modeling_bert_nlu.py
@@ -228,8 +228,8 @@ class BertNLUForJointUnderstanding(BertNLUForPreTraining):
         if intent_labels is not None and enumerable_entity_labels is not None:
             loss_fct = CrossEntropyLoss()
             loss_multi_label = MultiLabelSoftMarginLoss()
-            intent_loss = loss_fct(intent_logits.view(-1, self.num_intents), intent_labels.view(-1))
-            enumerable_entity_loss = loss_multi_label(enumerable_entity_logits.view(-1, self.num_enumerable_entities), enumerable_entity_labels.view(-1))
+            intent_loss = loss_fct(intent_logits.view(-1, self.num_intent_labels), intent_labels.view(-1))
+            enumerable_entity_loss = loss_multi_label(enumerable_entity_logits.view(-1, self.num_enumerable_entity_labels), enumerable_entity_labels.view(-1))
             non_enumerable_entity_loss = loss_fct(
                 non_enumerable_entity_logits.view(-1, self.num_non_enumerable_entity_labels),
                 non_enumerable_entity_labels.view(-1))
