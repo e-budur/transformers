5527148f6b10e3ae68258b084bbf7f4cd70a7ac5
Emrah Budur <emrah.budur@yahoo.com>
2019-11-14 23:34:54 +0300

    Fixing input processing in eval method of run_nlu.py

--- examples/run_nlu.py
+++ examples/run_nlu.py
@@ -241,9 +241,12 @@ def evaluate(args, model, tokenizer, prefix=""):
             batch = tuple(t.to(args.device) for t in batch)
 
             with torch.no_grad():
-                inputs = {'input_ids':      batch[0],
+                inputs = {'input_ids': batch[0],
                           'attention_mask': batch[1],
-                          'labels':         batch[3]}
+                          'intent_labels': batch[3],
+                          'enumerable_entity_labels': batch[4],
+                          'non_enumerable_entity_labels': batch[5]
+                          }
                 if args.model_type != 'distilbert':
                     inputs['token_type_ids'] = batch[2] if args.model_type in ['bert', 'xlnet'] else None  # XLM, DistilBERT and RoBERTa don't use segment_ids
                 outputs = model(**inputs)
