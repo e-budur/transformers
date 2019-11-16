880e2cca34f76b79218d58d3e9533bbe0198075b
Emrah Budur <emrah.budur@yahoo.com>
2019-11-16 23:52:32 +0300

    updated bert trainer file to support multi-gpu setting

--- examples/run_bert_pretrainer.py
+++ examples/run_bert_pretrainer.py
@@ -430,12 +430,15 @@ def main():
 			"Output directory ({}) already exists and is not empty. Use --overwrite_output_dir to overcome.".format(
 				args.output_dir))
 
-
-
 	# Setup CUDA, GPU & distributed training
 	if args.local_rank == -1 or args.no_cuda:
 		device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
 		args.n_gpu = torch.cuda.device_count()
+	else:  # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
+		torch.cuda.set_device(args.local_rank)
+		device = torch.device("cuda", args.local_rank)
+		torch.distributed.init_process_group(backend='nccl')
+		args.n_gpu = 1
 	args.device = device
 
 	logger.warning("torch.cuda.is_available(): %s, current device:%s", str(torch.cuda.is_available()), args.device)
