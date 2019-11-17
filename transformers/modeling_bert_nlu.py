92b2ebabb083abc1b6d1fd6f1f1ec933f55d68b3
Emrah Budur <emrah.budur@yahoo.com>
2019-11-17 19:46:27 +0300

    included BertNLUForPreTraining back

--- transformers/modeling_bert_nlu.py
+++ transformers/modeling_bert_nlu.py
@@ -69,6 +69,92 @@ class BertNLUPreTrainingHeads(original_bert.BertPreTrainingHeads):
        return language_model_prediction_scores, seq_relationship_score, multi_label_prediction_scores
 
 
+class BertNLUForPreTraining(BertPreTrainedModel):
+    r"""
+        **masked_lm_labels**: (`optional`) ``torch.LongTensor`` of shape ``(batch_size, sequence_length)``:
+            Labels for computing the masked language modeling loss.
+            Indices should be in ``[-1, 0, ..., config.vocab_size]`` (see ``input_ids`` docstring)
+            Tokens with indices set to ``-1`` are ignored (masked), the loss is only computed for the tokens with labels
+            in ``[0, ..., config.vocab_size]``
+        **next_sentence_label**: (`optional`) ``torch.LongTensor`` of shape ``(batch_size,)``:
+            Labels for computing the next sequence prediction (classification) loss. Input should be a sequence pair (see ``input_ids`` docstring)
+            Indices should be in ``[0, 1]``.
+            ``0`` indicates sequence B is a continuation of sequence A,
+            ``1`` indicates sequence B is a random sequence.
+        **multi_labels**: (`optional`) ``torch.LongTensor`` of shape ``(batch_size,)``:
+            Labels for computing the existence of particular characters prediction (multi-label classification) loss (bag of chars).
+            Indices should be in ``[0, 1]``.
+            ``0`` in i'th index of the output indicates the i'th character in the alphabet DOES NOT exist in any of the string A and B,
+            ``1`` in i'th index of the output indicates the i'th character in the alphabet DOES exist in any of the string A and B.
+    Outputs: `Tuple` comprising various elements depending on the configuration (config) and inputs:
+        **loss**: (`optional`, returned when both ``masked_lm_labels`` and ``next_sentence_label`` are provided) ``torch.FloatTensor`` of shape ``(1,)``:
+            Total loss as the sum of the masked language modeling loss and the next sequence prediction (classification) loss.
+        **prediction_scores**: ``torch.FloatTensor`` of shape ``(batch_size, sequence_length, config.vocab_size)``
+            Prediction scores of the language modeling head (scores for each vocabulary token before SoftMax).
+        **seq_relationship_scores**: ``torch.FloatTensor`` of shape ``(batch_size, 1)``
+            Prediction scores of the next sequence prediction (classification) head (scores of True/False continuation before SoftMax).
+        **multi_label_prediction_scores**: ``torch.FloatTensor`` of shape ``(batch_size, multi_label_output_size)``
+            Prediction scores of the existence of i'th letter in the alphabet head (bag of chars).
+        **hidden_states**: (`optional`, returned when ``config.output_hidden_states=True``)
+            list of ``torch.FloatTensor`` (one for the output of each layer + the output of the embeddings)
+            of shape ``(batch_size, sequence_length, hidden_size)``:
+            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+        **attentions**: (`optional`, returned when ``config.output_attentions=True``)
+            list of ``torch.FloatTensor`` (one for each layer) of shape ``(batch_size, num_heads, sequence_length, sequence_length)``:
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention heads.
+    Examples::
+        tokenizer = BertNLUTokenizer.from_pretrained('bert-base-uncased')
+        model = BertNLUForPreTraining.from_pretrained('bert-base-uncased')
+        input_ids = torch.tensor(tokenizer.encode("Hello, my dog is cute")).unsqueeze(0)  # Batch size 1
+        outputs = model(input_ids)
+        prediction_scores, seq_relationship_scores, multi_label_prediction_scores = outputs[:3]
+    """
+    def __init__(self, config):
+        super(BertNLUForPreTraining, self).__init__(config)
+
+        self.bert_nlu = BertNLUModel(config)
+        self.cls = BertNLUPreTrainingHeads(config)
+        self.config = config
+        self.init_weights()
+        self.tie_weights()
+        self.vocab_size = config.vocab_size
+
+    def tie_weights(self):
+        """ Make sure we are sharing the input and output embeddings.
+            Export to TorchScript can't handle parameter sharing so we are cloning them instead.
+        """
+        self._tie_or_clone_weights(self.cls.predictions.decoder,
+                                   self.bert_nlu.embeddings.word_embeddings)
+
+    def forward(self, input_ids, attention_mask=None, token_type_ids=None, position_ids=None, head_mask=None,
+                inputs_embeds=None,
+                masked_lm_labels=None, next_sentence_label=None, bag_of_tokens_label=None):
+
+        outputs = self.bert_nlu(input_ids,
+                            attention_mask=attention_mask,
+                            token_type_ids=token_type_ids,
+                            position_ids=position_ids,
+                            head_mask=head_mask,
+                            inputs_embeds=inputs_embeds)
+
+        sequence_output, multi_class_pooled_output, multi_label_pooled_output  = outputs[:3]
+        language_model_prediction_scores, seq_relationship_score, multi_label_prediction_scores = self.cls(sequence_output, multi_class_pooled_output, multi_label_pooled_output)
+
+        outputs = (language_model_prediction_scores, seq_relationship_score, multi_label_prediction_scores,) + outputs[3:]  # add hidden states and attention if they are here
+
+        if masked_lm_labels is not None and next_sentence_label is not None and bag_of_tokens_label is not None:
+            loss_fct = CrossEntropyLoss(ignore_index=-1)
+            loss_multi_label = MultiLabelSoftMarginLoss()
+            masked_lm_loss = loss_fct(language_model_prediction_scores.view(-1, self.config.vocab_size), masked_lm_labels.view(-1))
+            next_sentence_loss = loss_fct(seq_relationship_score.view(-1, 2), next_sentence_label.view(-1))
+            bag_of_tokens_loss = loss_multi_label(multi_label_prediction_scores.view(-1, self.vocab_size), bag_of_tokens_label.view(-1, self.vocab_size))
+            total_loss = masked_lm_loss + next_sentence_loss + bag_of_tokens_loss
+            outputs = (total_loss,) + outputs
+
+        return outputs  # (loss), prediction_scores, seq_relationship_score, multi_label_prediction_scores, (hidden_states), (attentions)
+
+
+
 class BertForJointUnderstanding(BertForPreTraining):
     def __init__(self, config):
         super(BertForJointUnderstanding, self).__init__(config)
