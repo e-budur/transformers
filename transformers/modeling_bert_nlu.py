1582e8865dd91572d0a6e2f1e192b96e7cfd8e1b
Emrah Budur <emrah.budur@yahoo.com>
2019-12-07 02:23:49 +0300

    substituted the current activation of BertMultiLabelPooler (sigmoid) for tanh

--- transformers/modeling_bert_nlu.py
+++ transformers/modeling_bert_nlu.py
@@ -12,7 +12,7 @@ class BertMultiLabelPooler(nn.Module):
     def __init__(self, config):
         super(BertMultiLabelPooler, self).__init__()
         self.dense = nn.Linear(config.hidden_size, config.hidden_size)
-        self.activation = nn.Sigmoid()
+        self.activation = nn.Tanh()
 
     def forward(self, hidden_states):
         # We "pool" the model by simply taking the hidden state corresponding
