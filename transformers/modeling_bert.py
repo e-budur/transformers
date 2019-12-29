bca33edc64018f6a268fa8d9c6baf5c4d41c30c8
Emrah Budur <emrah.budur@yahoo.com>
2019-12-29 14:38:43 +0300

    removing the printing step of the value of output_attentions in modeling_bert

--- transformers/modeling_bert.py
+++ transformers/modeling_bert.py
@@ -253,7 +253,6 @@ class BertSelfAttention(nn.Module):
             'queries': query_layer,
             'keys': key_layer
         }
-        print('self.output_attentions', self.output_attentions)
         outputs = (context_layer, attn_data) if self.output_attentions else (context_layer,)
         return outputs
 
