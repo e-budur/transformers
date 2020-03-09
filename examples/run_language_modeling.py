8c77b9a245febea360d8cbde48d09581171a571c
e-budur <emrah.budur@yahoo.com>
2020-03-08 21:15:32 -0700

    Enhanced progress print in each iteration

--- examples/run_language_modeling.py
+++ examples/run_language_modeling.py
@@ -427,8 +427,8 @@ def train(args, train_dataset, model: PreTrainedModel, tokenizer: PreTrainedToke
         epochs_trained, int(args.num_train_epochs), desc="Epoch", disable=args.local_rank not in [-1, 0]
     )
     set_seed(args)  # Added here for reproducibility
-    for _ in train_iterator:
-        epoch_iterator = tqdm(train_dataloader, desc="Iteration", disable=args.local_rank not in [-1, 0])
+    for epoch_num in train_iterator:
+        epoch_iterator = tqdm(train_dataloader, desc="Epoch: {}, Iteration :".format(epoch_num), disable=args.local_rank not in [-1, 0], miniters=int(len(train_dataloader)/50000))
         for step, batch in enumerate(epoch_iterator):
 
             # Skip past any already trained steps if resuming training
